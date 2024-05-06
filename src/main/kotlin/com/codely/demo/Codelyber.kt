6ad4a6bce0e2c82ec11bfb00dad58291fe874a94
UlloaVillar <edullvill@gmail.com>
2024-05-06 16:58:39 +0200

    Init

--- src/main/kotlin/com/codely/demo/Codelyber.kt
+++ src/main/kotlin/com/codely/demo/Codelyber.kt
@@ -1,9 +1,9 @@
 package com.codely.demo
-
-class Codelyber {
-    fun greet() = "Welcome to kotlin skeleton!!!"
-}
+import java.time.LocalDate
 
 fun main() {
-    println(Codelyber().greet())
+    println("Please enter a date with the format <yyyy-MM-DD>")
+    val readLine: String? = null
+    val input = LocalDate.parse(readLine ?: "2021-08-31")
+    println("You wrote $input ${input.dayOfWeek}")
 }
